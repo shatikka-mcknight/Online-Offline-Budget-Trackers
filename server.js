17687a4ef827dad85b866f56e614a70e072b63c5
Shatikka McKnight <32470118+shatikka-mcknight@users.noreply.github.com>
2021-07-17 18:05:34 -0400

    updated server.js

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
 app.use(logger("dev"));
 
 app.use(compression());
