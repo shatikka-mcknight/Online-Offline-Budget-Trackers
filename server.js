e51ce03016e7ccf183fb6c7393c8c8712c85b6c5
Shatikka McKnight <32470118+shatikka-mcknight@users.noreply.github.com>
2021-07-17 17:58:14 -0400

    update port server information

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
 const app = express();
 
 app.use(logger("dev"));
